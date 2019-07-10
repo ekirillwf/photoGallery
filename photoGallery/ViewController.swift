0aacab29ace18cd55c4c686e2322f91a16f93440
Kirill Elsufev <eysmen@mail.ru>
2019-07-10 11:50:32 +0700

    Initial Commit

---
+++ photoGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photoGallery
+//
+//  Created by Кирилл Елсуфьев on 10/07/2019.
+//  Copyright © 2019 Кирилл Елсуфьев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
